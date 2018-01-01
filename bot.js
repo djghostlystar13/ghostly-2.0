24fa6e4805faa58a9317c0c627b4b7197dac2037
djghostlystar13 <34558890+djghostlystar13@users.noreply.github.com>
2018-01-01 12:04:19 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,25 +8,25 @@ client.on(  +'ready', () => {
     
 client.on('message', message => {
     if (message.content === prefix +'help') {
-    	message.reply('Hello im Ghostly-2.0, my commands are the following with out quotation marks , bot commands about the menu ! , " $status " , " $s " , " Download " , " $features " , " $gta5-key " ,  Other bot commands! " $help " , " $ping " , " $bing " , " no " , " yes " ," hi " , " hello " , " hi " ," Bye " , " Goodbye " , " Goodnight " , " night ". ');
+    	message..reply('Hello im Ghostly-2.0, my commands are the following with out quotation marks , bot commands about the menu ! , " $status " , " $s " , " Download " , " $features " , " $gta5-key " ,  Other bot commands! " $help " , " $ping " , " $bing " . ');
   	}
 });
 //menu stuff 
 client.on('message', message => {
     if (message.content === 'download') {
-    	message.reply('the cheat is currently not available, please be patient as there is only one developer.');
+    	message.channel.reply('the cheat is currently not available, please be patient as there is only one developer.');
   	}
           if (message.content === 'Download') {
-            message.reply('the cheat is currently not available, please be patient as there is only one developer.');
+            message.channel.reply('the cheat is currently not available, please be patient as there is only one developer.');
         }
 });
 
 client.on('message', message => {
     if (message.content === prefix +'status') {
-    	message.reply('BOT = online, menu = Offline/detected.');
+    	message.channel.reply('BOT = online, menu = Offline/detected.');
   	}
         if (message.content === prefix +'s') {
-            message.reply('BOT = online, menu = Offline/detected.');
+            message.channel.reply('BOT = online, menu = Offline/detected.');
         }
     
 });
@@ -34,17 +34,17 @@ client.on('message', message => {
     client.on('message', message => {
     if (message.content === prefix +'gta5-key') {
     	 message.channel.send("Please check your **DM**s");
-        message.author.send('');
+        message.author.send('This code is your persional code do not share without quotation marks " Gta520172018 "');
   	}
 });
    */
 client.on('message', message => {
     if (message.content === prefix +'features') {
-    	message.reply('List of features in the menu! Godmode , DemiGod , *Teleport , No Reload , Infinite Ammo , Bullet Damage , RP increase , Never wanted , Super jump , Fast run ,Vehicle god , No recoil , Set time . *means this feature is buggy and sometimes crashes game . ');
+    	message.channel.reply('List of features in the menu! Godmode , DemiGod , *Teleport , No Reload , Infinite Ammo , Bullet Damage , RP increase , Never wanted , Super jump , Fast run ,Vehicle god , No recoil , Set time . *means this feature is buggy and sometimes crashes game . ');
   	}
 });
 
-//fun featues of bot
+/*fun featues of bot
 client.on('message', message => {
     if (message.content === 'No') {
     	message.reply('Yes!');
@@ -83,16 +83,16 @@ client.on('message', message => {
     	          message.reply('Hello!');
   	        }
 });
-
+*/
 client.on('message', message => {
     if (message.content === prefix +'ping') {
     	message.channel.send('PONG!');
   	}
        if (message.content === prefix +'bing') {
-    	message.reply('BONG!');
+    	message.channel.reply('BONG!');
   	}
 });
-
+/*
    client.on('message', message => {
     if (message.content === 'goodnight') {
     	message.reply('Goodnight, sleep well, talk to you again soon');
@@ -107,6 +107,6 @@ client.on('message', message => {
                     message.reply('Goodnight, sleep well, talk to you again soon');
                     }
     });
-
+*/
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
